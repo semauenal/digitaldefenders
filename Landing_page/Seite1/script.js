f00835eb3bc50050e861cf6a59de442ba8bcfd4a
sema.uenal <wi22040@lehre.dhbw-stuttgart.de>
2023-04-21 14:49:50 +0200

    Website Update

--- Landing_page/Seite1/script.js
+++ Landing_page/Seite1/script.js
@@ -3,7 +3,7 @@ function authenticate() {
 	var password = document.getElementById("password").value;
   var errorMessage = document.getElementById("errorMessage");
 	if (username === "test" && password === "1234") {
-		window.location.href = "https://example.com/other-page.html";
+		window.location.href = "http://127.0.0.1:3000/EndPage/indexEnd.html";
 	} else {
 		errorMessage.innerHTML = "Falscher Benutzername oder Passwort. Bitte versuchen Sie es erneut.";
 	}
