dd794ed6cc5cd28e803c8b4109b65625ccbab114
mascha.hosp <wi22058@lehre.dhbw-stuttgart.de>
2023-04-23 13:52:02 +0200

    neuer Code mit Display

--- Keypad/display_keypad.c
+++ Keypad/display_keypad.c
@@ -1,118 +1,82 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
-#include <Servo.h> 
- 
-// Pinlänge und Pincode festlegen
-const byte PINLENGTH = 4;
-char pinCode[PINLENGTH+1] = {'0','8','1','5'};
- 
-// Zwischenspeicher für Eingaben
-char keyBuffer[PINLENGTH+1] = {'-','-','-','-'};
- 
-// Kompakter kann man das auch so schreiben:
-// char pin[] = "0815";
-// char keyBuffer[] = "----";
- 
-// Definition für das Keypad von Elegoo
-const byte ROWS = 4; // vier Zeilen
-const byte COLS = 4; // vier Spalten
-// Symbole auf den Tasten
-char hexaKeys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+#include <Servo.h>
+
+// Pin Definitions
+#define GREEN_LED_PIN 12
+#define RED_LED_PIN 13
+#define SERVO_PIN 11
+
+// Set up servo object
+Servo doorServo;
+
+// Initialize keypad
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
-byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
- 
-// Einstellung für Servo und LEDs
-const int greenPin = 12;
-const int redPin = 13;
-const int servoPin = 11;
-const int angleClosed = 30;
-const int angleOpen = 120;
-const int openDelay = 3000;
- 
-// Servo-Objekt erzeugen
-Servo myservo;
- 
-// Instanz von Keypad erzeugen, Keymap und Pins übergeben
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
- 
-void setup(){
-  Serial.begin(9600);
-  // LED Pins setzen
-  pinMode(greenPin, OUTPUT);
-  pinMode(redPin, OUTPUT);
-  digitalWrite(greenPin, LOW);
-  digitalWrite(redPin, LOW);
-  // Servo auf Ausgangsposition
-  myservo.attach(servoPin);
-  myservo.write(angleClosed);
+byte rowPins[ROWS] = {2, 3, 4, 5};
+byte colPins[COLS] = {6, 7, 8, 9};
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+// Initialize LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Set password
+const char password[] = "1234";
+
+void setup() {
+  // Initialize pins
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  doorServo.attach(SERVO_PIN);
+
+  // Initialize LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Please enter");
+  lcd.setCursor(0, 1);
+  lcd.print("your password");
 }
- 
-void loop(){
-  // Gedrückte Taste abfragen
-  char customKey = customKeypad.getKey();
- 
-  if (customKey) {
-    // Check, ob ASCII Wert des Char einer Ziffer zwischen 0 und 9 entspricht
-    if ((int(customKey) >= 48) && (int(customKey) <= 57)){ 
-      addToKeyBuffer(customKey); 
+
+void loop() {
+  char key = keypad.getKey();
+
+  if (key) {
+    static int index = 0;
+    static char input[5];
+
+    if (key == '#') {
+      input[index] = '\0';
+      index = 0;
+
+      if (strcmp(input, password) == 0) {
+        lcd.clear();
+        lcd.print("Door opens");
+        digitalWrite(GREEN_LED_PIN, HIGH);
+        doorServo.write(90);
+      } else {
+        lcd.clear();
+        lcd.print("Incorrect");
+        digitalWrite(RED_LED_PIN, HIGH);
+        delay(1000);
+        digitalWrite(RED_LED_PIN, LOW);
+        lcd.clear();
+        lcd.print("Please enter");
+        lcd.setCursor(0, 1);
+        lcd.print("your password");
+      }
+    } else if (index < 4) {
+      input[index] = key;
+      index++;
+      lcd.setCursor(index - 1, 1);
+      lcd.print(key);
     }
-  // oder Code überprüfen, falls Raute gedrückt wurde 
-    else if (customKey == '#') { 
-      checkKey(); 
-    } 
-  } 
-} 
- 
-void addToKeyBuffer(char inkey) { 
-  Serial.print(inkey); 
-  Serial.print(" > ");
-  // Von links nach rechts Zeichen um eins weiterkopieren = ältestes Zeichen vergessen
-  for (int i=1; i<PINLENGTH; i++) {
-    keyBuffer[i-1] = keyBuffer[i];
-  }
-  // in ganz rechter Stelle die letzte Ziffer speichern
-  keyBuffer[PINLENGTH-1] = inkey;
-  Serial.println(keyBuffer);
-}
- 
-void checkKey() {
-  // Eingabe mit festgelegtem Pincode vergleichen
-  if (strcmp(keyBuffer, pinCode) == 0) {
-    Serial.println("CORRECT");
-    // Aktion für richtigen Pin ausführen
-    pinCorrect();
-  }
-  else {
-    Serial.println("WRONG!");
-    // Aktion für falschen Pin ausführen
-    pinWrong();
-  }
- 
-  // Nach Überprüfung Eingabe leeren
-  for (int i=0; i < PINLENGTH; i++) {
-    keyBuffer[i] = '-'; 
-  }
-}
- 
-// Aktion für korrekten Pincode
-void pinCorrect() {
-  myservo.write(angleOpen);
-  digitalWrite(greenPin, HIGH);
-  delay(openDelay);
-  myservo.write(angleClosed);
-  digitalWrite(greenPin, LOW);
-}
- 
-// Aktion für falschen Pincode
-void pinWrong() {
-  for (int i=0; i<3; i++) {
-    digitalWrite(redPin, HIGH);
-    delay(50);
-    digitalWrite(redPin, LOW);  
-    delay(20);
   }
 }
