4418d616581a2d4fb31a889f0400d7d79dda8dde
sema.uenal <wi22040@lehre.dhbw-stuttgart.de>
2023-05-10 15:28:54 +0200

    Landing Page mit Kommentaren

--- Landing_page/Seite1/script.js
+++ Landing_page/Seite1/script.js
@@ -1,10 +1,19 @@
 function authenticate() {
+	// Die Eingabe des Benutzernamens wird im Element mit der ID "username" gespeichert
 	var username = document.getElementById("username").value;
+	// Die Eingabe des Passworts wird im Element mit der ID "password" gespeichert
 	var password = document.getElementById("password").value;
-  var errorMessage = document.getElementById("errorMessage");
+	// Das Element mit der ID "errorMessage" wird in der Variable errorMessage gespeichert
+	var errorMessage = document.getElementById("errorMessage");
+  
+	// Wenn der eingegebene Benutzername "test" und das eingegebene Passwort "1234" ist
 	if (username === "test" && password === "1234") {
-		window.location.href = "http://127.0.0.1:3000/EndPage/indexEnd.html";
+	  // Leite den Benutzer auf die EndPage weiter
+	  window.location.href = "http://127.0.0.1:3000/EndPage/indexEnd.html";
 	} else {
-		errorMessage.innerHTML = "Falscher Benutzername oder Passwort. Bitte versuchen Sie es erneut.";
+	  // Andernfalls zeige eine Fehlermeldung an
+	  errorMessage.innerHTML =
+		"Falscher Benutzername oder Passwort. Bitte versuchen Sie es erneut.";
 	}
-}
+  }
+  
