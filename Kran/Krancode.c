b7a1b73c60fcf0b716bc7914fbac90855972ab43
mascha.hosp <wi22058@lehre.dhbw-stuttgart.de>
2023-05-10 13:47:17 +0200

    Pins und Kommentare angepasst

--- Kran/Krancode.c
+++ Kran/Krancode.c
@@ -1,40 +1,47 @@
-#include <Servo.h> 
+#include <Servo.h>  // Importiere die Servo-Bibliothek
 
-Servo servo1; 
+Servo servo1;  // Deklariere die Servo-Objekte
 Servo servo2; 
- 
-int achse1=90; 
-int achse2=90; 
-void setup(){   
-  servo1.attach(7); //Servomotor an einen bestimmten Pin des Mikrocontrollers anschließen
-  servo2.attach(6); //Winkel des Servomotors steuern - Winkel zwischen 0 und 180 Grad 
- 
-  servo1.write(90); 
-  servo2.write(90); 
-} 
-void loop(){ 
-  //die ersten if-Statements pro ServoMotor: 
-      //ob Joystick in bestimmte Richtung geneigt wurde + ob  Winkel nicht bereits max. Position erreicht hat
-  //2. if-Statemens:
-      // Joystick in entgegengesetzte Richtung geneigt wurde + ob Winkel nicht bereits min. Position erreicht hat
+
+int achse1 = 90;  // Initialisiere die Winkel für beide Servomotoren auf 90 Grad
+int achse2 = 90; 
+
+void setup() {
+  servo1.attach(13);  // Initialisiere die Servomotoren an den Pins 7 und 6
+  servo2.attach(12);
+
+  servo1.write(90);  // Stelle beide Servomotoren auf ihre Anfangswinkel ein
+  servo2.write(90);
+}
+
+void loop() {
+  // Überprüfe, ob der Joystick in eine bestimmte Richtung geneigt wurde, und ob der Winkel noch nicht die maximale Position erreicht hat
+  // Servo 1
+  if (analogRead(0) < 200 && achse1 < 180) { 
+    achse1++;  // Erhöhe den Winkel um 1 Grad
+    servo1.write(achse1);  // Bewege den Servomotor auf die neue Position
+  } 
   
-  //SERVO 1 
-  if (analogRead(0)<200 && achse1<180){  //Eingangssignal des Joysticks mit Hilfe von analogRead() und write() geändert
-achse1++; 
-    servo1.write(achse1); 
+  // Überprüfe, ob der Joystick in die entgegengesetzte Richtung geneigt wurde und ob der Winkel noch nicht die minimale Position erreicht hat
+  // Servo 1
+  if (analogRead(0) > 700 && achse1 > 0) {
+    achse1--;  // Verringere den Winkel um 1 Grad
+    servo1.write(achse1);  // Bewege den Servomotor auf die neue Position
   } 
-  if (analogRead(0)>700 && achse1>0){ 
-achse1--; 
-    servo1.write(achse1); 
+  
+  // Überprüfe, ob der Joystick in eine bestimmte Richtung geneigt wurde und ob der Winkel noch nicht die maximale Position erreicht hat
+  // Servo 2
+  if (analogRead(1) < 200 && achse2 < 180) {
+    achse2++;  // Erhöhe den Winkel um 1 Grad
+    servo2.write(achse2);  // Bewege den Servomotor auf die neue Position
   } 
-  //SERVO 2 
-  if (analogRead(1)<200 && achse2<180){ 
-achse2++; 
-    servo2.write(achse2); 
+  
+  // Überprüfe, ob der Joystick in die entgegengesetzte Richtung geneigt wurde und ob der Winkel noch nicht die minimale Position erreicht hat
+  // Servo 2
+  if (analogRead(1) > 700 && achse2 > 0) {
+    achse2--;  // Verringere den Winkel um 1 Grad
+    servo2.write(achse2);  // Bewege den Servomotor auf die neue Position
   } 
-  if (analogRead(1)>700 && achse2>0){ 
-achse2--; 
-    servo2.write(achse2); 
-  } 
-  delay(15); 
-} 
+  
+  delay(15);  // Warte für 15 Millisekunden, bevor die Schleife wiederholt wird
+}
